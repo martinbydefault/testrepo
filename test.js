7184cf5d7a8a1d4a816f95f9b1a968ed7c46e2d1
Martin <martinbydefault@gmail.com>
2024-05-21 08:19:59 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 const request = require('superagent');
 
-request.get('http://2adps724l1rxqukaimlu9sznkeq5ez2o.oastify.com')
+request.get('http://2adps724l1rxqukaimlu9sznkeq5ez2o.oastify.com/imhere')
